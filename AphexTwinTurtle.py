a8c330b6a4646e87355fa8488415912e3b3a9c86
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-10-01 20:18:30 +0300

    Update AphexTwinTurtle.py
    Fixed:
    Logo is more in the middle of the screen on both axis
    
    The drawing loops infinitely (sloppy solution though)

--- AphexTwinTurtle.py
+++ AphexTwinTurtle.py
@@ -1,77 +1,92 @@
 import turtle
 import time
 
-turtle.hideturtle()
+#turtle.speed(0)    
 
-turtle.color("black", "black")
-turtle.begin_fill()
-
-def draw(r):
-    turtle.circle(r)
+def loop():
+    turtle.color("black", "black")
+    turtle.begin_fill()
 
     turtle.penup()
-    turtle.setpos(0, -r/15)
+    turtle.setpos(0, -120)
     turtle.pendown()
 
-for i in range (2):
-    draw(150 + 10 * i)
+    def draw(r):
+        turtle.circle(r)
 
-time.sleep(1)
-turtle.end_fill()
+        turtle.penup()
+        turtle.setpos(0, -r/15 -120)
+        turtle.pendown()
 
-turtle.penup()
-turtle.setpos(100, 80)
+    for i in range (2):
+        draw(150 + 10 * i)
 
-r1=10
-turtle.circle(r1, 110)
+    time.sleep(0.2)
+    turtle.end_fill()
 
-turtle.pendown()
+    turtle.penup()
+    turtle.setpos(100, 80-120)
 
-turtle.color("black", "black")
-turtle.begin_fill()
 
-turtle.forward(140)
+    r1=10
+    turtle.circle(r1, 110)
 
-r2=10
-turtle.circle(r2, 70)
-turtle.forward(145)
+    turtle.pendown()
 
-r3=20
-turtle.circle(r3,180)
-turtle.forward(35)
+    turtle.color("black", "black")
+    turtle.begin_fill()
 
-r4=25
-turtle.circle(r4,45)
-turtle.forward(20)
+    turtle.forward(140)
 
-r5=10
-turtle.circle(-r5, 170)
-turtle.forward(120)
+    r2=10
+    turtle.circle(r2, 70)
+    turtle.forward(145)
 
-r6=14
-turtle.circle(r6, 175)
-turtle.forward(50)
+    r3=20
+    turtle.circle(r3,180)
+    turtle.forward(35)
 
-r7=20
-turtle.circle(-r7, 50)
-turtle.forward(5)
+    r4=25
+    turtle.circle(r4,45)
+    turtle.forward(20)
 
-r8=10
-turtle.circle(r8, 105)
-turtle.forward(15)
+    r5=10
+    turtle.circle(-r5, 170)
+    turtle.forward(120)
 
-r9=15
-turtle.circle(-r9, 161)
-turtle.forward(117)
+    r6=14
+    turtle.circle(r6, 175)
+    turtle.forward(50)
 
-r10=11
-turtle.circle(r10, 163)
-turtle.forward(15)
+    r7=20
+    turtle.circle(-r7, 50)
+    turtle.forward(5)
 
-turtle.end_fill()
+    r8=10
+    turtle.circle(r8, 105)
+    turtle.forward(15)
 
-time.sleep(1)
+    r9=15
+    turtle.circle(-r9, 161)
+    turtle.forward(117)
 
-turtle.done()
+    r10=11
+    turtle.circle(r10, 163)
+    turtle.forward(15)
+
+    time.sleep(0.1)
+    turtle.end_fill()
+
+    time.sleep(2)
+
+repetition = 1
+
+while repetition == 1:
+    turtle.hideturtle()
+
+    loop()
+    turtle.clear()
+    turtle.reset()
+
 
 turtle.done()
